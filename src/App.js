e5fa62febd55a9ca2941232d30de987809f83596
MateuVieira <mateus12v@hotmail.com>
2020-02-20 18:01:37 -0300

    Project using React Hooks

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect, useMemo, useCallback } from 'react';
 
-function App() {
+const App = () => {
+  const [techs, setTechs] = useState([]);
+  const [newTech, setNewTech] = useState('');
+
+  const handleAdd = useCallback(() => {
+    setTechs([...techs, newTech]);
+    setNewTech('');
+  }, [newTech, techs]);
+
+  useEffect(() => {
+    const storageTechs = localStorage.getItem('tech');
+
+    if (storageTechs) {
+      setTechs(JSON.parse(storageTechs));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('tech', JSON.stringify(techs));
+  }, [techs]);
+
+  const techSize = useMemo(() => techs.length, [techs]);
+
   return (
-    <h1>Hello World</h1>
+    <>
+      <ul>
+        {techs.map(tech => (<li key={tech} >{tech}</li>))}
+      </ul>
+      <strong>Você tem {techSize} tecnologias</strong>
+      <br />
+      <input value={newTech} onChange={e => setNewTech(e.target.value)} />
+      <button type='button' onClick={handleAdd} >Add</button>
+    </>
   );
 }
 
